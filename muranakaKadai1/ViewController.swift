e9f81c0c77c55335643e33ea97b3048043939722
akio0911 <akio0911@gmail.com>
2021-09-11 08:13:00 +0900

    feedback

--- muranakaKadai1/ViewController.swift
+++ muranakaKadai1/ViewController.swift
@@ -8,42 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet private weak var textFieldNum1: UITextField!
     @IBOutlet private weak var textFieldNum2: UITextField!
     @IBOutlet private weak var textFieldNum3: UITextField!
     @IBOutlet private weak var textFieldNum4: UITextField!
     @IBOutlet private weak var textFieldNum5: UITextField!
     @IBOutlet private weak var resultLabel: UILabel!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.textFieldNum1.keyboardType = UIKeyboardType.numberPad
-        self.textFieldNum2.keyboardType = UIKeyboardType.numberPad
-        self.textFieldNum3.keyboardType = UIKeyboardType.numberPad
-        self.textFieldNum4.keyboardType = UIKeyboardType.numberPad
-        self.textFieldNum5.keyboardType = UIKeyboardType.numberPad
-        // Do any additional setup after loading the view.
+
+        textFieldNum1.keyboardType = .numberPad
+        textFieldNum2.keyboardType = .numberPad
+        textFieldNum3.keyboardType = .numberPad
+        textFieldNum4.keyboardType = .numberPad
+        textFieldNum5.keyboardType = .numberPad
     }
-    
-    
-    
+
     @IBAction func additionButton(_ sender: Any) {
 
-        let num1 = Int(self.textFieldNum1.text!) ?? 0
-        let num2 = Int(self.textFieldNum2.text!) ?? 0
-        let num3 = Int(self.textFieldNum3.text!) ?? 0
-        let num4 = Int(self.textFieldNum4.text!) ?? 0
-        let num5 = Int(self.textFieldNum5.text!) ?? 0
-        
-        let result = num1 + num2 + num3 + num4 + num5
-        
-        resultLabel.text = "\(result)"
-        
-    }
-    
-    
-}
+        let num1 = Int(textFieldNum1.text!) ?? 0
+        let num2 = Int(textFieldNum2.text!) ?? 0
+        let num3 = Int(textFieldNum3.text!) ?? 0
+        let num4 = Int(textFieldNum4.text!) ?? 0
+        let num5 = Int(textFieldNum5.text!) ?? 0
 
+        let result = num1 + num2 + num3 + num4 + num5
+
+        resultLabel.text = "\(result)"
+
+    }
+
+}
